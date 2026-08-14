be52743f7b06416dda913597bdebc41090b386f2
Kevin Cho <kevincho@bwh.harvard.edu>
2026-08-14 14:55:03 -0400

    feat: prevent debug batch execution when create flag is set

--- lib/multi-shell-harmonization.py
+++ lib/multi-shell-harmonization.py
@@ -282,7 +282,8 @@ class multi_shell_pipeline(cli.Application):
             if self.debug and self.ref_csv:
                 print(f'\n## joining reconstructed reference: {self.reference} ##')
                 joinAllBshells(self.ref_csv, ref_bvals_file, 'reconstructed_', self.N_proc, self.force)
-                self.run_debug_batch(refListOutPrefix, tarListOutPrefix, ref_bvals)
+                if not self.create:
+                    self.run_debug_batch(refListOutPrefix, tarListOutPrefix, ref_bvals)
 
 if __name__== '__main__':
     multi_shell_pipeline.run()
